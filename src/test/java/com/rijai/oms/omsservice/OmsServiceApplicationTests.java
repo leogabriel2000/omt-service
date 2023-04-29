8cd06d0f1c250a8582308307db0d0c3f5f580e3f
rijai <rijaiitconsulting@gmail.com>
2023-04-29 16:00:49 +0800

    first commit

---
+++ src/test/java/com/rijai/oms/omsservice/OmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rijai.oms.omsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
